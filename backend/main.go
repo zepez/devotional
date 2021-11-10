ce17d90caa4968095d14f944bf194e3364aff6a7
Alex Zepezauer <zepezaueralex@gmail.com>
2021-11-10 13:45:06 -0500

    remove unneccessary put route

--- backend/main.go
+++ backend/main.go
@@ -12,8 +12,6 @@ func main() {
 	router.GET("/devotional", handler.GetDevotional)
 	router.GET("/devotionals", handler.GetDevotionals)
 
-	router.PUT("/devotional", handler.PutDevotional)
-
 	// default port 8080
 	// can be changed via env
 	router.Run()
