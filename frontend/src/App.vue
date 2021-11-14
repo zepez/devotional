d3578829c81e2a33415031609defda27c35a0fbe
Alex Zepezauer <zepezaueralex@gmail.com>
2021-11-14 15:54:04 -0500

    start vue/typescript via vite

---
+++ frontend/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <HomePage msg='Hello Vue 3 + TypeScript + Vite' />
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
+
+
+<script setup lang="ts">
+import HomePage from "./components/HomePage.vue";
+</script>
---
+++ frontend/src/components/HomePage.vue
@@ -0,0 +1,34 @@
+<template>
+	<div v-for="devotional in devotionals" :key="devotional.name">
+		<h1>{{ devotional.name }}</h1>
+		<p>{{ devotional.plain_text }}</p>
+	</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+interface Devotional {
+	name: string;
+	plain_text: string;
+}
+
+const devotionals = [
+	{
+		name: "testing1",
+		plain_text: "testing plain text"
+	},
+	{
+		name: "testing2",
+		plain_text: "testing plain text"
+	}
+];
+
+export default defineComponent({
+	data() {
+		return {
+			devotionals: devotionals as Devotional[]
+		};
+	}
+});
+</script>
