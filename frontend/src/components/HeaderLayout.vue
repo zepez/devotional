dbda37767ce5a3a4e9c1fb89b40fe86be3473327
Alex Zepezauer <zepezaueralex@gmail.com>
2021-11-17 16:15:42 -0500

    create and style navigation links

--- frontend/src/components/HeaderLayout.vue
+++ frontend/src/components/HeaderLayout.vue
@@ -1,12 +1,33 @@
 <template>
-	<div class="bg-white">
-		<h1 class="text-2xl text-center py-4 font-semibold">Vue.js Devotionals Application</h1>
+	<div class="bg-white pt-8 pb-6">
+		<h1 class="text-5xl text-center font-semibold mb-6">Vue.js Devotionals Application</h1>
 		<nav>
-			<ul class="flex">
-				<li class="">GitHub</li>
-				<li>lhm.org</li>
-				<li>Portfolio</li>
+			<ul class="flex justify-center flex-wrap">
+				<li v-for="link in navLinks" :key="link.name" class="mx-8 px-4 my-2 text-2xl border-2 border-gray-800">
+					<a :href="link.href" :target="link.external ? '_blank' : ''">
+						{{ link.name }}
+					</a>
+				</li>
 			</ul>
 		</nav>
 	</div>
 </template>
+
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+const navLinks = [
+	{ name: "GitHub", href: "https://github.com/zepez/devotional", external: true },
+	{ name: "lhm.org", href: "https://lhm.org", external: true },
+	{ name: "Portfolio", href: "https://zep.sh", external: true }
+];
+
+export default defineComponent({
+	data() {
+		return {
+			navLinks
+		};
+	}
+});
+</script>
