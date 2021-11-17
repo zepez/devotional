92027b914da431c9b5071e493e7a4b2eeb64b7ce
Alex Zepezauer <zepezaueralex@gmail.com>
2021-11-17 15:17:08 -0500

    add notice to footer and display target date on list view

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,8 @@
 <template>
 	<h1 class="text-2xl text-center py-4 font-semibold">Vue.js Devotionals Application</h1>
   <router-view />
+
+	<p class="text-center pb-4">All information sourced from <a href="https://lhm.org" target="_blank" class="underline">lhm.org</a></p>
 </template>
 
 <style>
