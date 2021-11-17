dc9350b627993c52088a36d26ae6999afc75819e
Alex Zepezauer <zepezaueralex@gmail.com>
2021-11-17 13:49:48 -0500

    clean ulist dipplay so that image is beside text

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,10 +1,12 @@
 <template>
 	<div class="container">
 		<ul class="-my-3 divide-y divide-gray-200">
-			<li v-for="devotional in devotionals" :key="devotional.name" class="py-3">
-				<img :src="devotional.image" :alt="devotional.name" />
-				<h4 class="font-semibold text-xl">{{ devotional.name }}</h4>
-				<p>{{ devotional.plain_text.substring(0, 100) + "..." }}</p>
+			<li v-for="devotional in devotionals" :key="devotional.name" class="py-3 grid grid-cols-6 gap-4">
+				<img :src="devotional.image" :alt="devotional.name" class="col-span-2 rounded-md"/>
+				<div class="col-span-4">
+					<h4 class="font-semibold text-2xl mb-2">{{ devotional.name }}</h4>
+					<p>{{ devotional.plain_text.substring(0, 150) + "..." }}</p>
+				</div>
 			</li>
 		</ul>
 	</div>
